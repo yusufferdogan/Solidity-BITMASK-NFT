3377ca946919d3494cca5533a6056756c19cc9a3
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2023-02-22 12:08:33 +0300

    make it erc721

---
+++ contracts/IPokemon.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+interface IPokemon {
+    error InvalidPokemons();
+    error MaxBirthReached();
+}
--- contracts/PokemonNFT.sol
+++ contracts/PokemonNFT.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.13;
 //import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./IPokemon.sol";
 
-// On-Chain NFT, SVG
-contract PokemonNFT {
+contract PokemonNFT is ERC721, ERC721Enumerable, IPokemon {
     uint8 public constant MAX_BIRTH = 4;
     uint256 private constant _BITMASK_ID = (1 << 64) - 1;
     uint256 private constant _BITMASK_HP = (1 << 16) - 1;
@@ -18,20 +21,20 @@ contract PokemonNFT {
     uint256 private constant _BITMASK_R1_GENE_POSITION = 176;
     uint256 private constant _BITMASK_R2_GENE_POSITION = 224;
 
-    uint64 public pokemonCounter = 1;
+    uint64 public pokemonCounter = 0;
+    IERC20 public immutable pokToken;
 
     // total 256 bit
     struct Pokemon {
         uint64 id;
         uint16 hp;
-        // 4 uint8 = 32 bit
         Property property;
-        //18 uint8 = 144 bit
         Gene dominant;
         Gene r1;
         Gene r2;
     }
 
+    // total 48 bit
     struct Gene {
         uint8 eyes;
         uint8 ears;
@@ -41,6 +44,7 @@ contract PokemonNFT {
         uint8 tail;
     }
 
+    //total 32 bit
     struct Property {
         uint8 childCount;
         uint8 feather;
@@ -49,30 +53,94 @@ contract PokemonNFT {
         uint8 pokemonType;
     }
 
-    // pokemon => Parents
-    mapping(uint64 => uint256[2]) public parents;
+    // pokemon ID => parents ID
+    mapping(uint256 => uint256[2]) public parents;
 
-    // pokemon => children(4 children in uint256)
-    mapping(uint256 => uint64[4]) public childrens;
+    // pokemon ID => children ID
+    mapping(uint256 => uint256[4]) public childrens;
 
-    // wallet => pokemons
-    mapping(address => uint256[]) public userPokemons;
+    // pokemon ID => Pokemon Owner
+    mapping(uint256 => address) public pokemonOwner;
 
+    //pokemon ID => pokemon
     mapping(uint256 => uint256) public pokemons;
 
-    constructor() {
-        userPokemons[msg.sender].push(1);
-        userPokemons[msg.sender].push(2);
+    constructor(address pokTokenAddress) ERC721("Foo721", "F721") {
+        pokToken = IERC20(pokTokenAddress);
+    }
+
+    function breedPokemon(uint64 pokemonID1, uint64 pokemonID2) public {
+        uint256 pokemon1 = pokemons[pokemonID1];
+        uint256 pokemon2 = pokemons[pokemonID2];
+        uint8 childCount1 = getChildCount(pokemon1);
+        uint8 childCount2 = getChildCount(pokemon2);
+        uint256 totalCost = computeBreedCost(childCount1) + computeBreedCost(childCount2);
+
+        if (pokemonOwner[pokemonID1] != msg.sender || pokemonOwner[pokemonID2] != msg.sender) revert InvalidPokemons();
+        if (childCount1 >= MAX_BIRTH || childCount2 >= MAX_BIRTH) revert MaxBirthReached();
+
+        uint256 id = uint256(++pokemonCounter);
+        (uint48 d, uint48 r1, uint48 r2) = computeGenes(pokemon1, pokemon2);
+        uint256 pokemon = createPokemon(id, d, r1, r2);
+
+        //set pokemon
+        pokemons[id] = pokemon;
+        //set owner of pokemon
+        pokemonOwner[id] = msg.sender;
+
+        //set parents of new breed pokemon
+        parents[id][0] = pokemonID1;
+        parents[id][1] = pokemonID2;
+
+        childrens[pokemonID1][childCount1] = id;
+        require(pokToken.transferFrom(msg.sender, address(this), totalCost), "Tx Error");
+    }
+
+    function computeBreedCost(uint8 childCount) private pure returns (uint256) {
+        if (childCount == 1) return 500;
+        else if (childCount == 2) return 1500;
+        else if (childCount == 3) return 3500;
+        else return 7500;
+    }
+
+    function computeGenes(uint256 pokemon1, uint256 pokemon2)
+        private
+        returns (
+            uint48 d,
+            uint48 r1,
+            uint48 r2
+        )
+    {}
+
+    function createPokemon(
+        uint256 id,
+        uint48 d,
+        uint48 r1,
+        uint48 r2
+    ) public view returns (uint256 pokemon) {
+        //uint256 id = uint256(++pokemonCounter);
+        uint256 hp = uint256(0xffff);
+        uint256 property = uint256(createProperty());
+        pokemon =
+            pokemon |
+            id |
+            (hp << _BITMASK_HP_POSITION) |
+            (property << _BITMASK_HP_CHILD_COUNT_POSITION) |
+            (uint256(d) << _BITMASK_DOMINANT_GENE_POSITION) |
+            (uint256(r1) << _BITMASK_R1_GENE_POSITION) |
+            (uint256(r2) << _BITMASK_R2_GENE_POSITION);
     }
 
     function getId(uint256 pokemon) public pure returns (uint256) {
-        //console.log(pokemon & _BITMASK_ID); // 0-63 first64Bit, print(6)
-        //console.logBytes(abi.encodePacked(pokemon & _BITMASK_ID)); // 0-63 first64Bit, print(6)
         return pokemon & _BITMASK_ID;
     }
 
     function getHp(uint256 pokemon) public pure returns (uint16) {
         return uint16((pokemon >> _BITMASK_HP_POSITION) & _BITMASK_HP);
+    }
+
+    function getChildCount(uint256 pokemon) public pure returns (uint8) {
+        return uint8((pokemon >> _BITMASK_HP_CHILD_COUNT_POSITION) & _BITMASK_GENE);
     }
 
     function getProperty(uint256 pokemon) public pure returns (Property memory property) {
@@ -125,7 +193,6 @@ contract PokemonNFT {
             (uint48(horn) << 24) |
             (uint48(mouth) << 32) |
             (uint48(tail) << 40);
-        //console.logBytes(abi.encodePacked(gene));
     }
 
     function createProperty() public view returns (uint32 property) {
@@ -134,30 +201,7 @@ contract PokemonNFT {
         uint32 body = uint32((block.timestamp * block.difficulty * pokemonCounter) % 16);
         uint32 pokemonType = uint32((block.timestamp * tx.gasprice * pokemonCounter) % 6);
         // solhint-enable not-rely-on-time
-
         property = property | (feather << 8) | (body << 16) | (pokemonType << 24);
-        //console.logBytes(abi.encodePacked(property));
-    }
-
-    function createPokemon(
-        uint48 d,
-        uint48 r1,
-        uint48 r2
-    ) public returns (uint256 pokemon) {
-        uint256 id = uint256(pokemonCounter++);
-        uint256 hp = uint256(0xffff);
-        uint256 property = uint256(createProperty());
-        pokemon =
-            pokemon |
-            id |
-            (hp << _BITMASK_HP_POSITION) |
-            (property << _BITMASK_HP_CHILD_COUNT_POSITION) |
-            (uint256(d) << _BITMASK_DOMINANT_GENE_POSITION) |
-            (uint256(r1) << _BITMASK_R1_GENE_POSITION) |
-            (uint256(r2) << _BITMASK_R2_GENE_POSITION);
-        userPokemons[msg.sender].push(id);
-        pokemons[id] = pokemon;
-        //console.logBytes(abi.encodePacked(pokemon));
     }
 
     function getPokemon(uint256 pokemon) public pure returns (bytes memory) {
@@ -165,8 +209,6 @@ contract PokemonNFT {
     }
 
     function incrementChildCount(uint256 pokemon) public pure returns (uint256) {
-        // (uint8 childCount, , , ) = getProperty(pokemon);
-        // check child count
         return pokemon + 2**80;
     }
 
@@ -227,5 +269,18 @@ contract PokemonNFT {
         );
 
         return string.concat(nft, nft2, nft3, nft4, "</svg>");
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        ERC721Enumerable._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return ERC721Enumerable.supportsInterface(interfaceId);
     }
 }
